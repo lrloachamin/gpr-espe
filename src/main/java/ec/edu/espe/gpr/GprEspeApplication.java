b10b81d3e600630cefb920bdda6508a3b0bb7463
lrloachamin <luisloachamin1128@gmail.com>
2022-11-01 15:46:53 -0500

    commit 2

--- src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
+++ src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
@@ -2,11 +2,15 @@ package ec.edu.espe.gpr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@CrossOrigin(origins= {"https://gpr-espe.azurewebsites.net"})
 @RestController
+@RequestMapping("/api/v1")
 public class GprEspeApplication {
 	
 	@GetMapping("/message")
