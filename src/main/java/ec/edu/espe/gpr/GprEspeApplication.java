0db31693cf4c83b002c73353eefa836961ae52fe
lrloachamin <luisloachamin1128@gmail.com>
2022-11-01 15:33:38 -0500

    Creacion 2

--- src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
+++ src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
@@ -1,13 +1,19 @@
 package ec.edu.espe.gpr;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GprEspeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GprEspeApplication.class, args);
+	
+	@GetMapping("/")
+	public String index() {
+		return "Prueba inicial";
 	}
+	
+
 
 }
