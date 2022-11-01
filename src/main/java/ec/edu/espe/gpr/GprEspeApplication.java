4e581e5809c537a3fc407ccd09ed5227f32fc43c
lrloachamin <luisloachamin1128@gmail.com>
2022-11-01 15:14:20 -0500

    Creacion del proyecto

---
+++ src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.gpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GprEspeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GprEspeApplication.class, args);
+	}
+
+}
