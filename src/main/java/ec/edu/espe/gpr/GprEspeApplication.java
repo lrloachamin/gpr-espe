2a93119c388a9e78f6d163ff46d061779e5cd108
omejia1997 <omardevaluado08@gmail.com>
2022-11-21 00:12:01 -0500

    regresando cambio

--- src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
+++ src/main/java/ec/edu/espe/gpr/GprEspeApplication.java
@@ -4,10 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import ec.edu.espe.gpr.services.IEmailService;
 
@@ -20,15 +17,6 @@ public class GprEspeApplication {
 	private IEmailService emservice;
 
 */
-@Bean
-public WebMvcConfigurer corsConfigurer() {
-	return new WebMvcConfigurer() {
-		@Override
-		public void addCorsMappings(CorsRegistry registry) {
-			registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT", "DELETE");
-		}
-	};
-}
 	public static void main(String[] args) {
 		SpringApplication.run(GprEspeApplication.class, args);
 		//System.out.println("Correo");
